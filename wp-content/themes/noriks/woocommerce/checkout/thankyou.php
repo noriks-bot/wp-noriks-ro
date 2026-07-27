973971d200e9b495b292753a5631e7753a69f579
Noriks (via Claude) <info@noriks.com>
2026-07-27 07:50:08 +0200

    Thankyou upsell: prave 3x/5x SIVE kompozitne slike (generirane iz produktne, isti stil kot crne) v temi (img/upsell), URL prek get_template_directory_uri

--- wp-content/themes/noriks/woocommerce/checkout/thankyou.php
+++ wp-content/themes/noriks/woocommerce/checkout/thankyou.php
@@ -57,8 +57,8 @@ if ( $upsell_is_tricouri ) {
     $upsell_qty_names  = array( 1 => '1x Boxeri Gri', 3 => '3x Boxeri Gri', 5 => '5x Boxeri Gri' );
     $upsell_qty_images = array(
         1 => 'https://noriks.com/ro/wp-content/uploads/2025/11/sive-boksarice-produktna.jpg',
-        3 => 'https://noriks.com/ro/wp-content/uploads/2025/11/sive-boksarice-produktna.jpg', // TODO: 3x siva kompozitna, ko obstaja
-        5 => 'https://noriks.com/ro/wp-content/uploads/2025/11/sive-boksarice-produktna.jpg', // TODO: 5x siva kompozitna, ko obstaja
+        3 => get_template_directory_uri() . '/img/upsell/siva-3x.png',
+        5 => get_template_directory_uri() . '/img/upsell/siva-5x.png',
     );
     $upsell_title_text = 'Adaugă boxeri acum – 50% reducere';
 }
